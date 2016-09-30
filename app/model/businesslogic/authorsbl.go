ce06655a85c072c1f56e4a23755dc3ed358c6334
Ramin Farajpour Cami <ramin.blackhat@gmail.com>
2016-09-30 09:49:11 +0330

    Add database model & connection gorm

--- app/model/businesslogic/authorsbl.go
+++ app/model/businesslogic/authorsbl.go
@@ -1,1 +1,14 @@
 package businesslogic
+
+import (
+	"github.com/raminfp/Bleta/app/model"
+	"github.com/raminfp/Bleta/database/drivers"
+)
+
+func Insert_Author(pmodel model.Authors) bool {
+	db := drivers.Connection()
+	defer db.Close()
+	db.AutoMigrate(&model.Authors{})
+	db.Create(&pmodel)
+	return true
+}
--- app/model/businesslogic/usersbl.go
+++ app/model/businesslogic/usersbl.go
@@ -5,13 +5,6 @@ import (
 	"github.com/raminfp/Bleta/database/drivers"
 )
 
-
-//
-//db := drivers.Connection()
-//defer db.Close()
-//user := Users{Firstname: "ramin", Lastname: "faraj"}
-//db.Create(&user)
-
 func Insert_Users(pmodel model.Users) bool {
 	db := drivers.Connection()
 	defer db.Close()
@@ -20,21 +13,4 @@ func Insert_Users(pmodel model.Users) bool {
 	return true
 
 }
-
-//def Insert_UsersLogs(self,pModel):
-//rep = Repository()
-//return rep.Insert(pModel)
-//
-//def Update_UsersLogs(self,pModel):
-//rep = Repository()
-//return rep.Update(pModel)
-//
-//def SelectOne_UsersLogs(self,pID):
-//rep = Repository()
-//return rep.SelectOne(Logs,pID)
-//
-//def SelectAll_UsersLogs(self):
-//rep = Repository()
-//return rep.SelectAll(Logs)
-
 
